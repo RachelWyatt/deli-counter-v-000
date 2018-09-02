8ae303da75284a517bb743f2b196b3f89b4ffaf0
Rachel Wyatt <rw126@evansville.edu>
2018-09-02 14:07:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size < 0
-    puts "Currently serving ."
+    puts "Currently serving #{katz_deli[1]}."
   else
     puts "There is nobody waiting to be served!"
   end
