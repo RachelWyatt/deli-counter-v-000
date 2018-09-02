89059cad82af871cea6ca03e44b0365aad5a162e
Rachel Wyatt <rw126@evansville.edu>
2018-09-02 13:50:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,12 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently XXX."
+    line_array = []
+    katz_deli.each_with_index do |name, index|
+      indexplusone = index + 1
+      line _array << "#{indexplusone}. #{name}"
   end
+  return line_array
 end
 
 def take_a_number(deli, name)
